9cf3c7dd42f6c7cbcf3cb859b374042e42bac7e7
Eduardo Barbieri <duduardo23@gmail.com>
2021-03-14 11:00:36 -0300

    Add redis client

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module httpproxy
 
-go 1.15
+go 1.16
+
+require (
+	github.com/go-redis/redis/v8 v8.7.1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"httpproxy/middleware"
 	"httpproxy/route"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
 )
 
 var routes map[string]route.Route
+var redisClient *redis.Client
 
 func getRequestURI(req *http.Request) string {
 	return req.RequestURI
@@ -34,14 +40,41 @@ func handlerRequest(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	routes = map[string]route.Route{}
+	initEnv()
+	initRedisClient()
 	initRoutes()
 	http.HandleFunc("/", handlerRequest)
 	log.Printf("Starting proxy in port %v", 8080)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
+func initEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func initRedisClient() {
+	redisAddress := os.Getenv("REDIS_ADDRESS")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+
+	log.Printf("initializing redis client for server %s...\n", redisAddress)
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: redisPassword, // no password set
+		DB:       0,             // use default DB
+	})
+	ctx := context.Background()
+	_, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("error connecting to redis server: %v", err)
+	}
+}
+
 func initRoutes() {
+	log.Println("initializing routes configurations...")
+	routes = map[string]route.Route{}
 	mids := []middleware.Middleware{
 		middleware.NewStatsMiddleware(),
 		middleware.NewRateLimitMiddleware(),
